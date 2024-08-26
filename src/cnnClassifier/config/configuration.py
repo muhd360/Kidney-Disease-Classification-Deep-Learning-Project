42039fe4b76317adff9a044a75ca714513b1f824
muhd360 <masheikh_b21@et.vjti.ac.in>
2024-08-26 23:45:54 +0530

    second commit

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -85,8 +85,8 @@ class ConfigurationManager:
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
-            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
-            mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
+            training_data="artifacts/data_ingestion/data",
+            mlflow_uri="https://dagshub.com/entbappy/data",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
